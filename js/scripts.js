c11e46390cab4be4ff12eb32b9d67ba2c2386585
cb@design-busse.de <DTxhruKb5q>
2019-02-04 15:42:40 +0100

    Update iOS File Download Problem

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,9 @@ function external(url) {
 
 function pdf(url) {
 	if (navigator.userAgent.match(/(Android)/)) {
+		if((url.substr(0, 2)) == "..") {
+			url = url.substr(3, 1000);
+		}
 		url = 'http://apps.apfel.gold/siteguide20/' + url;		
 		var ref = window.open(url, '_system', 'location=no,enableViewPortScale=yes');
 	} else {
