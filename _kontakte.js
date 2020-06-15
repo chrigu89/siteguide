a22516ff8f4b997222b35b9f0f7b36b8a2c78c10
chrigu89 <cb@design-busse.de>
2020-06-15 16:32:20 +0200

    2.2.1
    Update Downloads Update Kontakt

--- _kontakte.js
+++ _kontakte.js
@@ -1,9 +1,12 @@
 deutschland_plz = new Object();
 
 zustaendig_plz = new Object();
+
+
+
 //plz_='03, 10 - 33, 38 - 53, 57 - 59';
 
-
+/*
 name_='melanie_beckmann';
 zustaendig_plz[name_]= new Array(03,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,57,58,59);
 
@@ -17,9 +20,9 @@ deutschland_plz[name_]['fax']='';
 deutschland_plz[name_]['email']='<a href="mailto:melanie@beckmann@trelleborg.com">melanie@beckmann@<span class="orange_txt">trelleborg.com</span></a>';
 deutschland_plz[name_]['bild']='images/kontakt/de/melanie_beckmann.jpg';
 
+*/
 
-
-
+/*
 //plz_='34 - 37';
 name_='peter_malmer';
 zustaendig_plz[name_]= new Array(34,35,36,37);
@@ -32,13 +35,12 @@ deutschland_plz[name_]['fon']='<a href="tel:017610014290">+49(0)176 100 14290</a
 deutschland_plz[name_]['fax']='';
 deutschland_plz[name_]['email']='<a href="mailto:peter.malmer@trelleborg.com">peter.malmer@<span class="orange_txt">trelleborg.com</span></a>';
 deutschland_plz[name_]['bild']='images/kontakt/de/PM_6x8_4c-web.jpg';
-
-
+*/
 
 
 //01, 02, 04 - 09, 80 - 87, 90 - 99;
 name_='joerg_garbe';
-zustaendig_plz[name_]= new Array(01,02,04,05,06,07,08,09,80,81,82,83,84,85,86,87,90,91,92,93,94,95,96,97,98,99);
+zustaendig_plz[name_]= new Array(80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98);
 
 deutschland_plz[name_] = new Object();
 deutschland_plz[name_]['plz_land']='D-';
@@ -51,7 +53,7 @@ deutschland_plz[name_]['email']='<a href="mailto:jorg.garbe@trelleborg.com">jorg
 deutschland_plz[name_]['bild']='images/kontakt/de/JG_6x8_4c-web.jpg';
 
 
-
+/*
 //plz_=' 54 - 56, 60 - 79, 88, 89';
 name_='thomas_leschik';
 zustaendig_plz[name_]= new Array(54,55,56,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,88,89);
@@ -66,6 +68,40 @@ deutschland_plz[name_]['fax']='';
 deutschland_plz[name_]['email']='<a href="mailto:thomas.leschik@trelleborg.com">thomas.leschik@<span class="orange_txt">trelleborg.com</span></a>';
 deutschland_plz[name_]['bild']='images/kontakt/de/thomas_leschik.jpg';
 
+*/
+
+
+
+
+
+name_='thomas_schwartner';
+zustaendig_plz[name_]= new Array(26,32,33,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69);
+
+deutschland_plz[name_] = new Object();
+deutschland_plz[name_]['plz_land']='D-';
+deutschland_plz[name_]['ort']='47228 Duisburg';
+deutschland_plz[name_]['name']='Thomas Schwartner <br><span style="color: #000 !important; ">Gebietsverkaufsleiter Deutschland West</span> ';
+deutschland_plz[name_]['str']='Dr.-Alfred Herrhausen-Allee 36';
+deutschland_plz[name_]['fon']='<a href="tel:017610014290">+49 (0)176 10014290</a> <style>.tel_nr {background-position: left 0px !important;}</style>';
+deutschland_plz[name_]['fax']='';
+deutschland_plz[name_]['email']='<a href="mailto:thomas.schwartner@trelleborg.com">thomas.schwartner@<span class="orange_txt">trelleborg.com</span></a>';
+deutschland_plz[name_]['bild']='images/kontakt/de/thomas_schwartner.jpg';
+
+
+
+
+name_='ulrich_rudolph';
+zustaendig_plz[name_]= new Array(01,02,03,04,05,06,07,08,09,30,31,34,35,36,37,38,39,11,12,13,14,15,16,17,18,19,21,22,23,24,25,27,28,29,70,71,72,73,74,75,76,77,78,79,99);
+
+deutschland_plz[name_] = new Object();
+deutschland_plz[name_]['plz_land']='D-';
+deutschland_plz[name_]['ort']='47228 Duisburg';
+deutschland_plz[name_]['name']='Ulrich Rudolph <br><span style="color: #000 !important; ">Gebietsverkaufsleiter Deutschland</span> ';
+deutschland_plz[name_]['str']='Dr.-Alfred Herrhausen-Allee 36';
+deutschland_plz[name_]['fon']='<a href="tel:017610037380">+49 (0)176 10037380</a> <style>.tel_nr {background-position: left 0px !important;}</style>';
+deutschland_plz[name_]['fax']='';
+deutschland_plz[name_]['email']='<a href="mailto:ulrich.rudolph@trelleborg.com">ulrich.rudolph@<span class="orange_txt">trelleborg.com</span></a>';
+deutschland_plz[name_]['bild']='images/kontakt/de/ulrich_rudolph.jpg';
 
 
 
